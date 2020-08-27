4a062dd27e92e9e289598c05c55081e975c7c59b
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-08-27 10:39:42 -0300

    Update programa03.py

--- LTP2-2020-2/Pratica02/programa03.py
+++ LTP2-2020-2/Pratica02/programa03.py
@@ -1,4 +1,6 @@
+#Usuário fala dois números e nós calculamos a média deles
 numero1 = int(input("Número 1: "))
 numero2 = int(input("Número 2: "))
 media = (numero1 + numero2) / 2
+#O "\n" faz pular uma linha quando ele está no meio da string
 print("O resultado da média é: ", media)
