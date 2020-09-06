ca53f3c45e3827ed2c5cfa1fd7dbfe8bf85687c4
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-09-06 14:31:37 -0300

    Create programa04.py

---
+++ LTPC2-2020-2/Pratica02/programa04.py
@@ -0,0 +1,13 @@
+#Determinar se um número inteiro é par ou ímpar
+numero = int(input("Numero:"))
+#Calcula o resto da divisão de um número por 2
+resto = numero % 2
+
+#Condição
+if resto ==0:
+  print("PAR")
+  print("Vencedor")
+  print("Não desista de seus objetivos")
+  print("O mindset é a chave do sucesso")
+
+print("Esse sempre aparece!")
