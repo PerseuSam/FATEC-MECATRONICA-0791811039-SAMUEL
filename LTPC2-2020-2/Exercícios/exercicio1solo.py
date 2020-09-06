2f8856895408332e5bdf9c0b8b97abca33a482fe
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-09-06 14:09:32 -0300

    Delete exercicio1solo.py

--- LTPC2-2020-2/Exercícios/exercicio1solo.py
+++
@@ -1,7 +0,0 @@
-posição_inicial = int(input("Informe a Posição Inicial: "))
-velocidade = int(input("Informe a Velocidade: "))
-tempo = int(input("Informe o Instante de Tempo: "))
-
-Sf = posição_inicial + (velocidade * tempo)
-
-print("A Posição Final é:", Sf,"m")
