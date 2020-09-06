132a9e66b9fb5afedce2526b4417e18c5feb628b
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-09-06 14:47:43 -0300

    Update exercicio1_aula3.py

--- LTPC2-2020-2/Exercícios/exercicio1_aula3.py
+++ LTPC2-2020-2/Exercícios/exercicio1_aula3.py
@@ -1,6 +1,8 @@
-posição_inicial = int(input("Informe a Posição Inicial: "))
-velocidade = int(input("Informe a Velocidade: "))
-tempo = int(input("Informe o Instante de Tempo: "))
+#Determina o valor da Posição Final
+
+posição_inicial = int(input("Informe a Posição Inicial, em metros(m): "))
+velocidade = int(input("Informe a Velocidade, em metros por segundo(m/s): "))
+tempo = int(input("Informe o Instante de Tempo, em segundos(s): "))
 
 Sf = posição_inicial + (velocidade * tempo)
 
