b1a919370059349e05cb760d8f09204e64febbb1
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-09-17 11:05:59 -0300

    Create myproject.py

---
+++ LTPC2-2020-2/Pratica05/myproject.py
@@ -0,0 +1,24 @@
+temperaturas = []
+
+quantidade = int(input("Quantidade de temperaturas que deseja informar: "))
+
+while len(temperaturas) < quantidade:
+  temperatura = int(input('Temperatura:'))
+  temperaturas.append(temperatura)
+
+print('Temperaturas informadas: ', temperaturas)
+
+temperatura_media = sum(temperaturas)/len(temperaturas)
+print("Temperatura Média: ", temperatura_media)
+
+for temperatura in temperaturas:
+  if temperatura > temperatura_media:
+    print("Temperaturas acima da média: ", temperatura)
+
+#Encontrar o maior Valor
+maior = max(temperaturas)
+print('Maior valor:', maior)
+
+#Encontra o menor valor
+menor = min(temperaturas)
+print('Menor valor:', menor)
