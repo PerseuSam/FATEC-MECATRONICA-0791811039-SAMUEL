3aa4a2182e0384eaa1aa17dbbded9d1b594e5025
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-09-17 11:24:39 -0300

    Update myproject.py

--- LTPC2-2020-2/Pratica05/myproject.py
+++ LTPC2-2020-2/Pratica05/myproject.py
@@ -1,24 +1,43 @@
+#Criar um log de temperaturas
+#Ler N temperaturas
+#Calcular a temperatura media
+#Encontrar todas as temperaturas acima da media
+#Encontrar a maior temperatura
+#Encontrar a menor temperatura
+#################################################################################
+
+#Cria uma lista de temperaturas
 temperaturas = []
 
+#Permite adicionar uma quantidade temperaturas
 quantidade = int(input("Quantidade de temperaturas que deseja informar: "))
 
+#Adiciona as temperaturas
 while len(temperaturas) < quantidade:
   temperatura = float(input('Temperatura:'))
   temperaturas.append(temperatura)
 
-print('Temperaturas informadas: ', temperaturas)
-
+#Encontra a temperatura média
 temperatura_media = sum(temperaturas)/len(temperaturas)
-print("Temperatura Média: ", temperatura_media)
 
+#Encontra temperaturas acima da média
+temperaturas_acima_da_media = []
 for temperatura in temperaturas:
   if temperatura > temperatura_media:
-    print("Temperaturas acima da média: ", temperatura)
+    temperaturas_acima_da_media.append(temperatura)
 
-#Encontrar o maior Valor
+#Encontrar a maior temperatura
 maior = max(temperaturas)
-print('Maior temperatura:', maior)
 
-#Encontra o menor valor
+#Encontra a menor temperatura
 menor = min(temperaturas)
+
+
+
+
+
+print('Temperaturas informadas: ', temperaturas)
+print("Temperatura Média: ", temperatura_media)
+print("Temperaturas acima da média: ", temperaturas_acima_da_media)
+print('Maior temperatura:', maior)
 print('Menor temperatura:', menor)
