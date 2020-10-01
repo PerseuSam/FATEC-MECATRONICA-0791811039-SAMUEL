02ba08277e32500a2a3d1d8e475f45da21713db9
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-01 12:34:26 -0300

    Update exerci2.py

--- LTPC2-2020-2/Prova/exerci2.py
+++ LTPC2-2020-2/Prova/exerci2.py
@@ -1,19 +1,19 @@
-valores = []
+def exibirStatusLista(lista):
+  for valor in valores:
+    if valor > valor_medio:
+     acima_da_media.append(valor)
 
+valores = []
+acima_da_media = []
 quantidade = int(input("Quantidade de valores: "))
 
-while len(valores)<quantidade:
-  valor = int(input("Valor: "))
+while len(valores) < quantidade:
+  valor = int(input("Informe um valor:"))
   valores.append(valor)
-  
-valor_medio = sum(valores)/len(valores)
 
-acima_da_media = []
+valor_medio = sum(valores) / len(valores)
+exibirStatusLista(valores)
 
-for valor in valores:
-  if valor > valor_medio:
-    acima_da_media.append(valor)
-    
 print("Média: ", valor_medio)
 print("Valores informados: ", sorted(valores))
 print("Valores acima da média: ", sorted(acima_da_media))
