ab4435a8a245bdf3bb7ad67700588effa1c064bd
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-01 12:07:02 -0300

    Create exerci4.py

---
+++ LTPC2-2020-2/Prova/exerci4.py
@@ -0,0 +1,7 @@
+temperaturas = [34.5, 67.8, 89.7, 45.8, 23.45]
+
+temp_media = sum(temperaturas)/len(temperaturas)
+
+for temperatura in temperaturas:
+  if temperatura > temp_media:
+    print(temperatura)
