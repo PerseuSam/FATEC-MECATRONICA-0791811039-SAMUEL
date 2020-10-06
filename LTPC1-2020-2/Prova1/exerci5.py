e3e9800bb5c753d64de0eed148f5068c13d22e75
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-06 09:34:39 -0300

    Create exerci5.py

---
+++ LTPC1-2020-2/Prova1/exerci5.py
@@ -0,0 +1,12 @@
+ra = int(input("ra:"))
+if ra % 2 == 0:
+  valor = ra//2
+else:
+  valor = ra/2
+
+valor =  valor ** 2 + ra % 3 - 0.01 ** 4.67
+
+if valor > 0:
+  valor = 3 ** (5-2)
+
+print(valor)
