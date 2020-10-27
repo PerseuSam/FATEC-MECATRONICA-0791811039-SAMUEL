fcb6e2aca27f275cb950f5f33b6b01d445ea8cf7
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-27 09:20:08 -0300

    Update proj.py

--- LTPC1-2020-2/Projeto Final/proj.py
+++ LTPC1-2020-2/Projeto Final/proj.py
@@ -1,1 +1,18 @@
 print("Olá")
+print("O que deseja hoje?")
+print("Para adicionar usuário - Digite 1 ")
+print("Para realizar transação - Digite 2")
+
+continuar = True
+opcao = 0
+
+while continuar:
+  opcao = input("Opção: ")
+  if opcao == "1":
+    print("adicionando")
+    continuar = False
+  elif opcao == "2":
+    print("entrando")
+    continuar = False
+  else:
+    print("Opção inválida, tente novamente")
