ee4cb68b76c9991dc4c45b8ae516a4f50b5f2946
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-27 09:48:59 -0300

    Update proj.py

--- LTPC1-2020-2/Projeto Final/proj.py
+++ LTPC1-2020-2/Projeto Final/proj.py
@@ -1,3 +1,5 @@
+#Inicializção
+
 print("Olá")
 print("O que deseja hoje?")
 print("Para adicionar usuário - Digite 1 ")
@@ -9,10 +11,21 @@ opcao = 0
 while continuar:
   opcao = input("Opção: ")
   if opcao == "1":
-    print("adicionando")
+    print("Vamos lá")
     continuar = False
   elif opcao == "2":
     print("entrando")
     continuar = False
   else:
     print("Opção inválida, tente novamente")
+
+#Opção 1
+nome_completo = input("Vamos começar pelo seu nome, qual seu nome completo? ").split()
+print("Olá", nome_completo[0],)
+
+mail = input("Agora digite seu e-mail: ")
+
+print("Estamos quase lá")
+
+senha_0 = input("Digite uma senha: ")
+
