b7fa5f4d9f794c28830eb093ebf7c903e4353335
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-28 20:36:25 -0300

    Update proj1.py

--- LTPC1-2020-2/Projeto Final/proj1.py
+++ LTPC1-2020-2/Projeto Final/proj1.py
@@ -1,8 +1,24 @@
+##BOM, NÃO MEXER
+
+contas = {1:1000, 2:250, 3:3000}
+
+dinheiro = int(input("coloca dinheiro: "))
+ID = int(input("Informe o ID:"))
+  #Verifica se a categoria já existe no dicionario
+if ID in contas.keys():
+  #Categoria já existe
+  contas[ID] += dinheiro
+  print(contas)
+else:
+  #Uma nova categoria vaio ser cadastrada
+  contas[ID] = dinheiro
+  print(contas)
+
+
+
+
+##BOM, NÃO MEXER
 usuarios = {"1":["Carlos Augusto", "augusto.carlos@gmail.com", "2358"], "2":["Ana Carolina", "ana.carlo@gmail.com", "6752"], "3":["Igor Moraes", "imoraes.raes@gmail.com", "1374"]}
-
-contas = {"1":[1000], "2":[250], "3":[3000]}
-
-import random
 
 
 
@@ -14,6 +30,7 @@ if codigo == "QR":
   nome = input("Insira seu nome: ").title()
   valor = int(input("Insira o valor: "))
   ID = int(input("Seu ID: "))
+  import random
   aleatorio = (random.randrange(1000, 9999))
   
   memoria = []
@@ -25,15 +42,3 @@ if codigo == "QR":
   print(memoria)
 else:
   print("deu ruim")
-
-
-#Para pagar
-
-print("Olá")
-usuario = input("informe o usuário: ").title()
-valor = float(input("Informe o valor: "))
-QR_code = input("informe o QR Code: ")
-
-print(usuario)
-print(valor)
-print(QR_code)
