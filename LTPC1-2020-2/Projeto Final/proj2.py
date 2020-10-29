f8062cc36764624f77d555e0360b7df81e06dfa8
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-29 19:01:12 -0300

    Update proj2.py

--- LTPC1-2020-2/Projeto Final/proj2.py
+++ LTPC1-2020-2/Projeto Final/proj2.py
@@ -2,18 +2,24 @@ nomes = ["Julia", "Davi", "Diego"]
 e_mails = ["ju@gmail", "davi@gmail", "ego@gmail"]
 senhas = ["2312", "4782", "3572"]
 
-um_nome = input("Coloque seu nome: ")
-senha = input("Coloque sua senha: ")
 
-elemento = nomes.index(um_nome) # MOSTRA A POSIÇÃO DA PALAVRA DENTRO DA LISTA
-print(elemento)
+repetir = True
+while repetir:
+
+  um_nome = input("Coloque seu nome: ")
+  senha = input("Coloque sua senha: ")
+
+  ID = nomes.index(um_nome) # MOSTRA A POSIÇÃO DA PALAVRA DENTRO DA LISTA
+  print(ID)
 
 
-if um_nome in nomes:
-  if senha in senhas[elemento]:
-    print("Acesso autorizado")
-  else:
-    print("Senha ou nome incorreto")
+  if um_nome in nomes:
+    if senha in senhas[ID]:
+      print("Acesso autorizado")
+      repetir = False
+    else:
+      print("Senha ou nome incorreto")
+      print("Tente novamente\n")
 
 
 
