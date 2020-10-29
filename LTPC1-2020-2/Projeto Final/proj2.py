93149ea165595f74ec6f27b557ad93699b0edda7
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-29 19:34:04 -0300

    Update proj2.py

--- LTPC1-2020-2/Projeto Final/proj2.py
+++ LTPC1-2020-2/Projeto Final/proj2.py
@@ -1,7 +1,7 @@
 nomes = ["Julia", "Davi", "Diego"]
 e_mails = ["ju@gmail", "davi@gmail", "ego@gmail"]
 senhas = ["1234", "2345", "3456"]
-saldo = [1000.00, 250.00, 3000.00]
+saldo = [1000, 250, 3000]
 
 
 repetir = True
@@ -12,8 +12,8 @@ while repetir:
 
   if um_nome in nomes:
     ID = nomes.index(um_nome) # MOSTRA A POSIÇÃO DA PALAVRA DENTRO DA LISTA
-    print(ID)
-    if senha in senhas[ID]:
+    print(ID)#não mostra para o usuário
+    if senha in senhas[ID]:#CORRIGIR
       print("Acesso autorizado")
       repetir = False
     else:
@@ -23,17 +23,19 @@ while repetir:
     print("Senha ou nome incorreto")
     print("Tente novamente\n")
 
-print("deu bom")
+print("deu bom")#não mostra para o usuário
 
 um_nome = input("QR Code aqui, nome: ")
 recebedor= nomes.index(um_nome)
 
-print(saldo)
+print(saldo)#não mostra para o usuário
 valor = int(input("Digite um valor: "))
 
-if (saldo[ID] - valor) > 0:
+if (saldo[ID] - valor) >= 0:
   saldo[ID] -= valor
   saldo[recebedor] += valor
-  print(saldo)
+  print(saldo[ID])
+  print(saldo)#não mostra para o usuário
 else:
   print("saldo insuficiente")
+  print("Seu saldo é de: ", "R$ ",saldo[ID])
