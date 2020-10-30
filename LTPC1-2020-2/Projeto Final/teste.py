c3f4c2471272171d82ac0e6a6e7d0ed9cacbd3c3
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-29 22:12:18 -0300

    Create teste.py

---
+++ LTPC1-2020-2/Projeto Final/teste.py
@@ -0,0 +1,17 @@
+Listas dentro de um dicionário
+
+Usuarios = {}
+Usuarios[1] = "Carlos Augusto", "augusto.carlos@gmail.com", "2358"
+Usuarios[2] = "Ana Carolina", "ana.carlo@gmail.com", "6752"
+Usuarios[3] = "Igor Moraes", "imoraes.raes@gmail.com", "1374"
+
+
+nome = input("Coloca um nome: ")
+
+
+if nome in Usuarios[1]:
+  print("Dahora")
+else:
+  print("Ruim")
+
+print(Usuarios)
