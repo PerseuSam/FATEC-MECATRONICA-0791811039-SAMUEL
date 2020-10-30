778e6b69f93a97e95bb5ec359f8ae687117d7165
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-10-30 01:27:25 -0300

    Update QRprog.py

--- LTPC1-2020-2/Projeto Final/QRprog.py
+++ LTPC1-2020-2/Projeto Final/QRprog.py
@@ -7,7 +7,7 @@ primeira_posicao = 0
 segunda_posicao = 0
 terceira_posicao = 0
 nominho = 0
-senhazinha = 0
+valorzinho = 0
 
 posicao_do_pontoEvirgula = 0
 while posicao < len(QR_code):
@@ -16,19 +16,25 @@ while posicao < len(QR_code):
     posicao_do_pontoEvirgula = posicao
     if marcar == 1:
       primeira_posicao = posicao_do_pontoEvirgula
-      print("passou aqui")
     if  marcar == 2:
-      print("passou aqui")
       segunda_posicao = posicao_do_pontoEvirgula
       nominho = QR_code[(primeira_posicao + 2) : (segunda_posicao - 1)]
       print(nominho)
 
     if marcar == 3:
-      print("passou aqui")
       terceira_posicao = posicao_do_pontoEvirgula
-      senhazinha = QR_code[(segunda_posicao + 2) : (terceira_posicao - 1)]
-      print(senhazinha)
-  print(marcar)
+      valorzinho = QR_code[(segunda_posicao + 2) : (terceira_posicao - 1)]
+      print(valorzinho)
   posicao = posicao + 1
 
+um_nome = input("Insira um nome(DAVI): ").upper()
+valor = int(input("Insira um valor(325): "))
+
+if nominho == um_nome:
+  print("nome correto")
+  if valor == valorzinho:
+    print("valor correto")
+else:
+  print("Dados diferentes")
+  
 #1 ; DAVI ; 325 ; 5155
