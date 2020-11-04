221a60cbd9f7fe0ece9f413d782943d9f9272abd
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-11-04 20:06:58 -0300

    Update cadastra.py

--- LTPC1-2020-2/Projeto Final/cadastra.py
+++ LTPC1-2020-2/Projeto Final/cadastra.py
@@ -1,17 +1,55 @@
 #print(" ".join(nome_completo)) #QUANDO PRECISAR MOSTAR O NOME INTEIRO SEM SEPARAÇÃO
+#---------------------------------- 1) CRIAR CONTA -------------------------------------
+
+def cria_conta():
+
+  continuar_2 = True
+  while continuar_2:
+    nome_completo = (input("\nVamos começar pelo seu nome, qual seu nome completo? ").title()).split()
+    print("Olá", nome_completo[0].title())
+    e_mail = input("Agora digite seu e-mail: ") #CORRIGIR  
+    print("Estamos quase lá")
+    senha = input("Digite uma senha: ") #CORRIGIR  
+
+    usuarios.append(" ".join(nome_completo))
+    e_mails.append(e_mail)
+    senhas.append(senha)
+    valor = int(input("Insira um valor inicial: "))
+    saldo.append(valor)
+
+    print("\nUsuários") #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
+    print(usuarios) #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
+
+    print("\nE-mails") #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
+    print(e_mails) #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
+
+    print("\nSenhas") #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
+    print(senhas) #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
+
+    print("\nSaldos") #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
+    print(saldo) #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
+
+    continuar_2 = input("\nAdicionar mais algúem? (S/N)").upper() == "S"
+
+
+
+
 #--------------------------------------- INICIO ----------------------------------------
 
 usuarios = ["Ana Lima", "Davi Reis", "Diego Jota"]
 e_mails = ["a_lima@gmail", "davi&rei@gmail", "ego&jota@gmail"]
 senhas = ["1234", "2345", "3456"]
 saldo = [1000, 250, 3000]
+
+
+
 #-------------------------------------- ABERTURA ---------------------------------------
 
 print("Olá!")
 print("Em que posso ajudá-lo hoje?\n")
 
-programa = True
-while programa:
+programa_completo = True
+while programa_completo:
   
   continuar_1 = True
   while continuar_1:
@@ -21,33 +59,7 @@ while programa:
     opcao = 0
     opcao = input("Opção: ") #CORRIGIR   
     if opcao == "1": #CADASTRO DE PESSOAS NOVAS
-      continuar_2 = True
-      while continuar_2:
-        nome_completo = (input("\nVamos começar pelo seu nome, qual seu nome completo? ").title()).split()
-        print("Olá", nome_completo[0].title())
-        e_mail = input("Agora digite seu e-mail: ") #CORRIGIR  
-        print("Estamos quase lá")
-        senha = input("Digite uma senha: ") #CORRIGIR  
-
-        usuarios.append(" ".join(nome_completo))
-        e_mails.append(e_mail)
-        senhas.append(senha)
-        valor = int(input("Insira um valor inicial: "))
-        saldo.append(valor)
-
-        print("\nUsuários") #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
-        print(usuarios) #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
-
-        print("\nE-mails") #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
-        print(e_mails) #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
-
-        print("\nSenhas") #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
-        print(senhas) #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
-
-        print("\nSaldos") #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
-        print(saldo) #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
-
-        continuar_2 = input("\nAdicionar mais algúem? (S/N)").upper() == "S"
+      cria_conta()
       continuar_1 = False
 
     elif opcao == "2": #ENTRA EM TRANSAÇÕES (ETAPA FEITA EM "aPrincipal.py")
@@ -57,7 +69,7 @@ while programa:
     elif opcao == "sair":
       print("Até breve") #SAI DO PROGRAMA TODO
       continuar_1 = False
-      programa = False
+      programa_completo = False
 
     else:
       print("Opção inválida")
