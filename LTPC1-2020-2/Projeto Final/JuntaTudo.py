c17de98ee87abf43ec8edacf87dcbf07f148f776
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2020-11-04 20:48:32 -0300

    Update JuntaTudo.py

--- LTPC1-2020-2/Projeto Final/JuntaTudo.py
+++ LTPC1-2020-2/Projeto Final/JuntaTudo.py
@@ -71,13 +71,97 @@ def transacao():
 
     if escolha == "P":
       print("Você escolheu a opção Pagamento") #(CASO 1: PAGAMENTO)
+      repete_pagar = True
+      while repete_pagar:
+        verificaNome = True
+        while verificaNome:
+          recebedor = input("\nPara quem você deseja pagar?(Nome completo) ").title()
+          if recebedor in usuarios:
+            verificaNome = False
+          else:
+            print("Algo errado")
+            print("Tente novamente")
 
-      #pagar()*********************************************************************************
+        verificaValor = True
+        while verificaValor:
+          valor = input("Insira o valor: ")
+          if valor.isdigit():
+            valor = int(valor)
+            verificaValor = False
+          else:
+            print("Algo errado")
+            print("Tente novamente")
 
+        QR_code = input("Insira o QR Code: ") #CORRIGIR?, se digita algo dierente do QR Code, da crash
+
+        ID_recebedor = usuarios.index(recebedor)
+        primeiro_nome = recebedor.split()
+        primeiro_nome[0].upper()
+
+        marcar = 0
+        posicao = 0
+        primeira_posicao = 0
+        segunda_posicao = 0
+        terceira_posicao = 0
+        ID_QR = 0
+        recebedor_QR = 0
+        valor_QR = 0
+        posicao_do_pontoEvirgula = 0
+        while posicao < len(QR_code):
+          if QR_code[posicao] == ";":
+            marcar += 1
+            posicao_do_pontoEvirgula = posicao
+            if marcar == 1:
+              primeira_posicao = posicao_do_pontoEvirgula
+              ID_QR = int(QR_code[:primeira_posicao])
+            if  marcar == 2:
+              segunda_posicao = posicao_do_pontoEvirgula
+              recebedor_QR = (QR_code[(primeira_posicao+1) : segunda_posicao]).title()
+            if marcar == 3:
+              terceira_posicao = posicao_do_pontoEvirgula
+              valor_QR = int(QR_code[(segunda_posicao+1) : terceira_posicao])
+          posicao = posicao + 1
+
+
+        if recebedor == nome_completo or primeiro_nome[0].upper() != recebedor_QR.upper() or ID_recebedor != ID_QR:
+          print("Algo deu errado")
+          print("Tente novamente")
+        elif valor != valor_QR:
+          print("Valor informado diferente do QR Code")
+          print("Tente novamente")
+        else:
+          posicao_recebedor = usuarios.index(recebedor) 
+          
+        if (saldo[ID] - valor) >= 0:
+          saldo[ID] -= valor
+          saldo[posicao_recebedor] += valor
+          print("\nTransação efetuada")
+          print("Seu saldo atual é: R$", saldo[ID])
+          print("Saldo de todos os clientes:", saldo, "\n") #NÃO MOSTRAR PARA O USUÁRIO, SOMENTE PARA TESTE
+          repete_pagar = False
+        else:
+          print("Saldo insuficiente")
+          print("Seu saldo é de: R$ ",saldo[ID])
+          saldo_zero = True
+        while saldo_zero:
+          print("Tentar novamente ou sair?")
+          print("Para Tentar Novamente - Digite 'T'")
+          print("Para sair - Digite 'SAIR'")
+          decisao = 0
+          decisao = input("Opção: ").upper()
+          if decisao == "T":
+            saldo_zero = False
+            repete_pagar = True
+          elif decisao == "SAIR":
+            saldo_zero = False
+            repete_pagar = False
+          else:
+            print("Escolha Inválida")
+            print("Tente Novamente\n")
       PR = False
+
     elif escolha == "R":
       print("Você escolheu a opção Receber") #(CASO 2: RECEBIMENTO)
-
       verificaNumero = True
       while verificaNumero:
         valor = input("\nGerar QR Code de qual valor? ")
@@ -96,6 +180,7 @@ def transacao():
     elif escolha == "VOLTAR":
       print("Você escolheu a opção voltar\n") 
       PR = False
+
     else:
       print("Opção inválida")
       print("Tente Novamente")
