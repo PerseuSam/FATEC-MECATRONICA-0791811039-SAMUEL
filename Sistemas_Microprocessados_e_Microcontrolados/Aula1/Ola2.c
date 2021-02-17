1414e6632b99aaa6e5b06be23868263661ee36e8
PerseuSam <69973689+PerseuSam@users.noreply.github.com>
2021-02-17 08:22:30 -0300

    Create Ola2.c

---
+++ Sistemas_Microprocessados_e_Microcontrolados/Aula1/Ola2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Olá Mundo\n");
+    // Comentário de uma linha
+    /*
+        *Comentário de mais de uma linha
+        *Colocar um "*" para falar é bem comum
+        *1
+        *2
+    */
+    // O "/n" pula linha
+    printf("Meu nome é Samuel");
+    return 0;
+}
